742dcc1eb08d59379d93bbff55af898dcc953cce
Anthony Platanios <anthonyplat719791@hotmail.com>
2014-06-21 14:41:52 -0400

    Fixed some tests.

--- test/org/platanios/learn/combination/ErrorRatesEstimationTest.java
+++ test/org/platanios/learn/combination/ErrorRatesEstimationTest.java
@@ -8,16 +8,30 @@ import org.junit.Test;
  */
 public class ErrorRatesEstimationTest {
     @Test
-    public void testAgreementRatesVector() {
-        String filename = "/Users/Anthony/Development/GitHub/learn/data/combination/brain/labels/region_1.csv";
+    public void testErrorRatesEstimationVector() {
+        String filename = ErrorRatesEstimationTest.class.getResource("resources/food_labels.csv").getPath();
         String separator = ",";
         boolean[][] observations = DataPreprocessing.getObservationsFromCsvFile(filename, separator);
-        filename = "/Users/Anthony/Development/GitHub/learn/data/combination/brain/sample error rates/region_1.csv";
+        filename = ErrorRatesEstimationTest.class.getResource("resources/food_sample_error_rates.csv").getPath();
         double[] sampleErrorRates = DataPreprocessing.getSampleErrorRatesFromCsvFile(filename);
 
-        ErrorRatesEstimation ere = new ErrorRatesEstimation(new AgreementRatesVector(8, 8, observations), 8, 8);
+        ErrorRatesEstimation ere = new ErrorRatesEstimation(new AgreementRatesVector(4, 4, observations, true), 4, 4);
         double[] obtainedErrorRates = ere.solve().errorRates;
 
-        Assert.assertTrue(true);
+        double mad = 0.0;
+        double mad_ind = 0.0;
+
+        for (int i = 0; i < obtainedErrorRates.length; i++) {
+            mad += Math.abs(sampleErrorRates[i] - obtainedErrorRates[i]);
+            if (i < observations[0].length) {
+                mad_ind += Math.abs(sampleErrorRates[i] - obtainedErrorRates[i]);
+            }
+        }
+
+        mad /= sampleErrorRates.length;
+        mad_ind /= observations[0].length;
+
+        Assert.assertEquals(mad, 0.0038168724169213237, 1E-18);
+        Assert.assertEquals(mad_ind, 0.008144776534951827, 1E-18);
     }
 }
--- test/org/platanios/math/combinatorics/CombinatoricsUtilitiesTest.java
+++ test/org/platanios/math/combinatorics/CombinatoricsUtilitiesTest.java
@@ -13,18 +13,19 @@ import java.util.List;
 public class CombinatoricsUtilitiesTest {
     @Test
     public void testGetCombinations() {
-        List<ArrayList<Integer>> obtainedResult = CombinatoricsUtilities.getCombinations(5, 3);
-        List<ArrayList<Integer>> correctResult = new ArrayList<ArrayList<Integer>>();
-        correctResult.add(new ArrayList<Integer>(Arrays.asList(0, 1, 2)));
-        correctResult.add(new ArrayList<Integer>(Arrays.asList(0, 1, 3)));
-        correctResult.add(new ArrayList<Integer>(Arrays.asList(0, 1, 4)));
-        correctResult.add(new ArrayList<Integer>(Arrays.asList(0, 2, 3)));
-        correctResult.add(new ArrayList<Integer>(Arrays.asList(0, 2, 4)));
-        correctResult.add(new ArrayList<Integer>(Arrays.asList(0, 3, 4)));
-        correctResult.add(new ArrayList<Integer>(Arrays.asList(1, 2, 3)));
-        correctResult.add(new ArrayList<Integer>(Arrays.asList(1, 2, 4)));
-        correctResult.add(new ArrayList<Integer>(Arrays.asList(1, 3, 4)));
-        correctResult.add(new ArrayList<Integer>(Arrays.asList(2, 3, 4)));
-        Assert.assertTrue(obtainedResult.equals(correctResult));
+        int[][] obtainedResult = CombinatoricsUtilities.getCombinations(5, 3);
+        int[][] correctResult = new int[][] {
+                { 0, 1, 2 },
+                { 0, 1, 3 },
+                { 0, 1, 4 },
+                { 0, 2, 3 },
+                { 0, 2, 4 },
+                { 0, 3, 4 },
+                { 1, 2, 3 },
+                { 1, 2, 4 },
+                { 1, 3, 4 },
+                { 2, 3, 4 }
+        };
+        Assert.assertArrayEquals(obtainedResult, correctResult);
     }
 }
