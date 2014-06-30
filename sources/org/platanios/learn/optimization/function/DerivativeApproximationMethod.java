b8a0923d657e84d698435f30321d1fc9ab5a435e
Anthony Platanios <anthonyplat719791@hotmail.com>
2014-06-30 02:06:38 -0400

    Implemented several derivative approximation methods.

---
+++ sources/org/platanios/learn/optimization/function/DerivativeApproximationMethod.java
@@ -0,0 +1,10 @@
+package org.platanios.learn.optimization.function;
+
+/**
+ * @author Emmanouil Antonios Platanios
+ */
+public enum DerivativeApproximationMethod {
+    FORWARD_DIFFERENCE,
+    /** Much more accurate than the forward-difference method (O(&epsilon;^2) estimation error instead of O(&epsilon;)). */
+    CENTRAL_DIFFERENCE
+}
