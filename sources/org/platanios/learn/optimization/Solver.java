58ac76a6fb1f1f824273eeeb1eecfcdebc6f9827
Anthony Platanios <anthonyplat719791@hotmail.com>
2014-07-13 18:11:11 -0400

    Minor change.

--- sources/org/platanios/learn/optimization/Solver.java
+++ sources/org/platanios/learn/optimization/Solver.java
@@ -7,7 +7,7 @@ import java.text.DecimalFormat;
 /**
  * @author Emmanouil Antonios Platanios
  */
-interface Solver {
+public interface Solver {
     static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("0.0000000000E0");
 
     public Vector solve();
