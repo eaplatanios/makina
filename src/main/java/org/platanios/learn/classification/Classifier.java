fcac4b350eb7533d3c2208dc90708569880e317a
Anthony Platanios <anthonyplat719791@hotmail.com>
2014-10-15 22:41:10 -0400

    Minor bug fix.

--- src/main/java/org/platanios/learn/classification/Classifier.java
+++ src/main/java/org/platanios/learn/classification/Classifier.java
@@ -6,6 +6,6 @@ import org.platanios.learn.math.matrix.Vector;
  * @author Emmanouil Antonios Platanios
  */
 public interface Classifier<T extends Vector, S> {
-    public double predict(DataInstance<T, S> dataInstance) throws UntrainedModelException;
-    public double[] predict(DataInstance<Vector, Integer>[] dataInstances) throws UntrainedModelException;
+    public double predict(DataInstance<T, S> dataInstance);
+    public double[] predict(DataInstance<Vector, Integer>[] dataInstances);
 }
