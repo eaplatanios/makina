4c723cd25d052ed698d091bf2841bc164fb1d652
Anthony Platanios <anthonyplat719791@hotmail.com>
2014-11-21 05:06:15 -0500

    Fixed the Hibernate configuration.

--- trade/src/main/java/org/platanios/trade/data/HibernateUtilities.java
+++ trade/src/main/java/org/platanios/trade/data/HibernateUtilities.java
@@ -12,26 +12,27 @@ class HibernateUtilities {
     private static SessionFactory sessionFactory = buildSessionFactory();
 
     protected static SessionFactory buildSessionFactory() {
-        Configuration configuration =
-                new Configuration()
-                        .setProperty("hibernate.connection.driver_class", "org.mariadb.jdbc.Driver")
-                        .setProperty("hibernate.connection.url", "jdbc:mariadb://localhost")
-                        .setProperty("hibernate.connection.username", "root")
-                        .setProperty("hibernate.connection.password", "")
-                        .setProperty("hibernate.connection.pool_size", "1")
-                        .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
-                        .setProperty("hibernate.current_session_context_class", "thread")
-                        .setProperty("hibernate.show_sql", "false")
-                        .setProperty("hibernate.id.new_generator_mappings", "true")
-                        .setProperty("hibernate.hbm2ddl.auto", "update")
-                        .addAnnotatedClass(DailyStockData.class)
-                        .addAnnotatedClass(DataVendor.class)
-                        .addAnnotatedClass(Exchange.class)
-                        .addAnnotatedClass(Industry.class)
-                        .addAnnotatedClass(IndustryGroup.class)
-                        .addAnnotatedClass(Sector.class)
-                        .addAnnotatedClass(Stock.class)
-                        .addAnnotatedClass(SubIndustry.class);
+        Configuration configuration = new Configuration().configure();
+//        Configuration configuration =
+//                new Configuration()
+//                        .setProperty("hibernate.connection.driver_class", "org.mariadb.jdbc.Driver")
+//                        .setProperty("hibernate.connection.url", "jdbc:mariadb://localhost")
+//                        .setProperty("hibernate.connection.username", "root")
+//                        .setProperty("hibernate.connection.password", "")
+//                        .setProperty("hibernate.connection.pool_size", "1")
+//                        .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+//                        .setProperty("hibernate.current_session_context_class", "thread")
+//                        .setProperty("hibernate.show_sql", "false")
+//                        .setProperty("hibernate.id.new_generator_mappings", "true")
+//                        .setProperty("hibernate.hbm2ddl.auto", "update")
+//                        .addAnnotatedClass(DailyStockData.class)
+//                        .addAnnotatedClass(DataVendor.class)
+//                        .addAnnotatedClass(Exchange.class)
+//                        .addAnnotatedClass(Industry.class)
+//                        .addAnnotatedClass(IndustryGroup.class)
+//                        .addAnnotatedClass(Sector.class)
+//                        .addAnnotatedClass(Stock.class)
+//                        .addAnnotatedClass(SubIndustry.class);
         ServiceRegistry serviceRegistry =
                 new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
         sessionFactory = configuration.buildSessionFactory(serviceRegistry);
