800567f2f8c039efd65fc46d71a0dad99a83ad5a
Anthony Platanios <anthonyplat719791@hotmail.com>
2015-01-25 23:12:44 -0500

    Minor re-organization.

--- learn/src/test/java/org/platanios/learn/classification/MultiClassLogisticRegressionTest.java
+++ learn/src/test/java/org/platanios/learn/classification/MultiClassLogisticRegressionTest.java
@@ -3,6 +3,14 @@ package org.platanios.learn.classification;
 import org.junit.Assert;
 import org.junit.Test;
 import org.platanios.learn.math.MathUtilities;
+import org.platanios.learn.math.matrix.Vector;
+import org.platanios.learn.math.matrix.Vectors;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Emmanouil Antonios Platanios
@@ -11,7 +19,7 @@ public class MultiClassLogisticRegressionTest {
     @Test
     public void testLogisticRegressionUsingSGDWithConstantStepSize() {
         String filename = MultiClassLogisticRegressionTest.class.getResource("/FishersIris.csv").getPath();
-        TrainingData data = DataPreprocessing.parseLabeledDataFromCSVFile(filename);
+        TrainingData data = parseLabeledDataFromCSVFile(filename);
         MultiClassLogisticRegression classifier = new MultiClassLogisticRegression.Builder(data)
                 .stochastic(true)
 //                .batchSize(5)
@@ -44,7 +52,7 @@ public class MultiClassLogisticRegressionTest {
 //    @Test
 //    public void testLogisticRegressionUsingSGDWithScaledStepSize() {
 //        String filename = LogisticRegressionTest.class.getResource("/FishersIris.csv").getPath();
-//        TrainingData data = DataPreprocessing.parseLabeledDataFromCSVFile(filename);
+//        TrainingData data = parseLabeledDataFromCSVFile(filename);
 //        LogisticRegression classifier = new LogisticRegression.Builder(data)
 //                .stochastic(true)
 //                .batchSize(5)
@@ -76,7 +84,7 @@ public class MultiClassLogisticRegressionTest {
     @Test
     public void testLogisticRegressionUsingBFGS() {
         String filename = MultiClassLogisticRegressionTest.class.getResource("/FishersIris.csv").getPath();
-        TrainingData data = DataPreprocessing.parseLabeledDataFromCSVFile(filename);
+        TrainingData data = parseLabeledDataFromCSVFile(filename);
         MultiClassLogisticRegression classifier = new MultiClassLogisticRegression.Builder(data)
                 .stochastic(false)
                 .largeScale(false)
@@ -105,7 +113,7 @@ public class MultiClassLogisticRegressionTest {
     @Test
     public void testLogisticRegressionUsingLBFGS() {
         String filename = MultiClassLogisticRegressionTest.class.getResource("/FishersIris.csv").getPath();
-        TrainingData data = DataPreprocessing.parseLabeledDataFromCSVFile(filename);
+        TrainingData data = parseLabeledDataFromCSVFile(filename);
         MultiClassLogisticRegression classifier = new MultiClassLogisticRegression.Builder(data)
                 .stochastic(false)
                 .largeScale(true)
@@ -129,5 +137,54 @@ public class MultiClassLogisticRegressionTest {
         }
         int[] expectedPredictions = new int[] { 1, 0, 2, 1, 0, 1, 2, 0, 1, 0 };
         Assert.assertArrayEquals(expectedPredictions, actualPredictions);
+    }
+
+    public static TrainingData parseLabeledDataFromCSVFile(String filename) {
+        String separator = ",";
+
+        int numberOfFeatures;
+        List<Integer> labels = new ArrayList<>();
+        List<Vector> data = new ArrayList<>();
+
+        BufferedReader br = null;
+        String line;
+        String[] classifiersNames = null;
+
+        try {
+            br = new BufferedReader(new FileReader(filename));
+            line = br.readLine();
+            classifiersNames = line.split(separator);
+            numberOfFeatures = classifiersNames.length - 1;
+            while ((line = br.readLine()) != null) {
+                String[] outputs = line.split(separator);
+                labels.add(Integer.parseInt(outputs[0]));
+                double[] dataSample = new double[numberOfFeatures];
+                for (int i = 0; i < numberOfFeatures; i++) {
+                    dataSample[i] = Double.parseDouble(outputs[i+1]);
+                }
+                data.add(Vectors.dense(dataSample));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        if (data.size() != labels.size()) {
+            throw new IllegalArgumentException("The number of data labels and data samples does not match."); // TODO: Maybe the IllegalArgumentException is not the most appropriate here.
+        }
+
+        List<TrainingData.Entry> trainingData = new ArrayList<>();
+        for (int i = 0; i < data.size(); i++) {
+            trainingData.add(new TrainingData.Entry(data.get(i), labels.get(i)));
+        }
+
+        return new TrainingData(trainingData);
     }
 }
