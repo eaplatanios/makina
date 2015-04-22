41efe029e926d1d79f002d376e8f63d4783176b2
Anthony Platanios <anthonyplat719791@hotmail.com>
2015-04-22 01:47:36 -0400

    Minor variable naming change.

--- learn/src/test/java/org/platanios/learn/optimization/NewtonSolverTest.java
+++ learn/src/test/java/org/platanios/learn/optimization/NewtonSolverTest.java
@@ -14,19 +14,19 @@ public class NewtonSolverTest {
     @Test
     public void testNewtonSolver() {
         System.out.println("Rosenbrock Function Newton Solver:\n");
-        NewtonSolver newtonRaphsonSolver =
+        NewtonSolver newtonSolver =
                 new NewtonSolver.Builder(new RosenbrockFunction(),
                                          Vectors.dense(new double[]{-1.2, 1})).build();
-        double[] actualResult = newtonRaphsonSolver.solve().getDenseArray();
+        double[] actualResult = newtonSolver.solve().getDenseArray();
         double[] expectedResult = new double[] { 1, 1 };
         Assert.assertArrayEquals(expectedResult, actualResult, 1e-2);
 
         System.out.println("Quadratic Function Newton Solver:\n");
         Matrix A = new Matrix(new double[][] { { 1, 0.5 }, { 0.5, 1 } });
         Vector b = Vectors.dense(new double[]{1, 2});
-        newtonRaphsonSolver = new NewtonSolver.Builder(new QuadraticFunction(A, b),
+        newtonSolver = new NewtonSolver.Builder(new QuadraticFunction(A, b),
                                                        Vectors.dense(new double[]{0, 0})).build();
-        actualResult = newtonRaphsonSolver.solve().getDenseArray();
+        actualResult = newtonSolver.solve().getDenseArray();
         expectedResult = new double[] { 0, 2 };
         Assert.assertArrayEquals(expectedResult, actualResult, 1e-2);
     }
