2a1c7ad9028ef6c029fbf7ee8ad77af09078e33a
eaplatanios <eap719791!>
2015-04-21 21:32:09 -0400

    Added a few more unit tests for the consensus ADMM solver.

--- learn/src/test/java/org/platanios/learn/optimization/ConsensusAlternatingDirectionsMethodOfMultipliersSolverTest.java
+++ learn/src/test/java/org/platanios/learn/optimization/ConsensusAlternatingDirectionsMethodOfMultipliersSolverTest.java
@@ -1,13 +1,11 @@
 package org.platanios.learn.optimization;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.platanios.learn.math.matrix.Matrix;
 import org.platanios.learn.math.matrix.Vector;
 import org.platanios.learn.math.matrix.Vectors;
-import org.platanios.learn.optimization.ConsensusAlternatingDirectionsMethodOfMultipliersSolver;
+import org.platanios.learn.optimization.constraint.LinearEqualityConstraint;
 import org.platanios.learn.optimization.function.LinearFunction;
-import org.platanios.learn.optimization.function.QuadraticFunction;
 import org.platanios.learn.optimization.function.SumFunction;
 
 
@@ -16,44 +14,81 @@ import org.platanios.learn.optimization.function.SumFunction;
  */
 public class ConsensusAlternatingDirectionsMethodOfMultipliersSolverTest {
     @Test
-    public void testNewtonSolver() {
-        System.out.println("Consensus ADMM Solver:\n");
-
-        // Setup the world's simplest problem
-        // C = 1; D = 0; C >> A; A >> B; D >> B
-        // 1 - A
-        LinearFunction lf_1mA = new LinearFunction(Vectors.dense(new double[]{-1}), 1);
-        // A - B
-        LinearFunction lf_AmB = new LinearFunction(Vectors.dense(new double[]{1, -1}), 0);
-        // 0 - B
-        LinearFunction lf_0mB = new LinearFunction(Vectors.dense(new double[]{-1}), 0);
-
-        // Create a sum function that indexes the variables (A=0, B=1)
-        SumFunction sumFunction = new SumFunction.Builder(2).addTerm(lf_1mA,
-                new int[] { 0 }).addTerm(lf_AmB, new int[] {0,1}).addTerm(lf_0mB, new int[] {1}).build();
+    public void testSimpleConsensusADMM1() {
+        SumFunction sumFunction = new SumFunction.Builder(2)
+                .addTerm(new LinearFunction(Vectors.dense(new double[]{-1}), 1), 0)
+                .addTerm(new LinearFunction(Vectors.dense(new double[]{1, -1}), 0), 0, 1)
+                .addTerm(new LinearFunction(Vectors.dense(new double[]{-1}), 0), 1)
+                .build();
 
         ConsensusAlternatingDirectionsMethodOfMultipliersSolver consensusADMMSolver =
                 new ConsensusAlternatingDirectionsMethodOfMultipliersSolver.Builder( sumFunction,
-                        Vectors.dense(new double[]{0.5,0.5})).build();
+                        Vectors.dense(new double[]{0.5,0.5}))
+                        .maximumNumberOfIterations(100)
+                        .checkForObjectiveConvergence(false)
+                        .checkForGradientConvergence(false)
+                        .checkForPointConvergence(false)
+                        .loggingLevel(5)
+                        .build();
 
         Vector result = consensusADMMSolver.solve();
-        System.out.println(result.get(0));   // Should be 1.0
-        System.out.println(result.get(1));   // Should be 1.0
+        System.out.println(result.get(0));
+        System.out.println(result.get(1));
         System.out.println('\n');
+    }
 
-        /*
-        double[] actualResult = newtonRaphsonSolver.solve().getDenseArray();
-        double[] expectedResult = new double[] { 1, 1 };
-        Assert.assertArrayEquals(expectedResult, actualResult, 1e-2);
+    @Test
+    public void testSimpleConsensusADMM2() {
+        SumFunction sumFunction = new SumFunction.Builder(2)
+                .addTerm(new LinearFunction(Vectors.dense(new double[]{-1}), 1), 0)
+                .addTerm(new LinearFunction(Vectors.dense(new double[]{1, -1}), 0), 0, 1)
+                .addTerm(new LinearFunction(Vectors.dense(new double[]{-1}), 0), 1)
+                .addTerm(new LinearFunction(Vectors.dense(new double[]{1}), -1), 0)
+                .addTerm(new LinearFunction(Vectors.dense(new double[]{-1, 1}), 0), 0, 1)
+                .addTerm(new LinearFunction(Vectors.dense(new double[]{1}), 0), 1)
+                .build();
 
-        System.out.println("Quadratic Function Newton Solver:\n");
-        Matrix A = new Matrix(new double[][] { { 1, 0.5 }, { 0.5, 1 } });
-        Vector b = Vectors.dense(new double[]{1, 2});
-        newtonRaphsonSolver = new NewtonSolver.Builder(new QuadraticFunction(A, b),
-                Vectors.dense(new double[]{0, 0})).build();
-        actualResult = newtonRaphsonSolver.solve().getDenseArray();
-        expectedResult = new double[] { 0, 2 };
-        Assert.assertArrayEquals(expectedResult, actualResult, 1e-2);
-        */
+        ConsensusAlternatingDirectionsMethodOfMultipliersSolver consensusADMMSolver =
+                new ConsensusAlternatingDirectionsMethodOfMultipliersSolver.Builder( sumFunction,
+                        Vectors.dense(new double[]{0.5,0.5}))
+                        .maximumNumberOfIterations(100)
+                        .checkForObjectiveConvergence(false)
+                        .checkForGradientConvergence(false)
+                        .checkForPointConvergence(false)
+                        .loggingLevel(5)
+                        .build();
+
+        Vector result = consensusADMMSolver.solve();
+        System.out.println(result.get(0));
+        System.out.println(result.get(1));
+        System.out.println('\n');
+    }
+
+    @Test
+    public void testSimpleConstrainedConsensusADMM2() {
+        SumFunction sumFunction = new SumFunction.Builder(2)
+                .addTerm(new LinearFunction(Vectors.dense(new double[]{-1}), 1), 0)
+                .addTerm(new LinearFunction(Vectors.dense(new double[]{1, -1}), 0), 0, 1)
+                .addTerm(new LinearFunction(Vectors.dense(new double[]{-1}), 0), 1)
+                .addTerm(new LinearFunction(Vectors.dense(new double[]{1}), -1), 0)
+                .addTerm(new LinearFunction(Vectors.dense(new double[]{-1, 1}), 0), 0, 1)
+                .addTerm(new LinearFunction(Vectors.dense(new double[]{1}), 0), 1)
+                .build();
+
+        ConsensusAlternatingDirectionsMethodOfMultipliersSolver consensusADMMSolver =
+                new ConsensusAlternatingDirectionsMethodOfMultipliersSolver.Builder(sumFunction, Vectors.dense(new double[]{0.5,0.5}))
+                        .addConstraint(new int[]{0, 1}, new LinearEqualityConstraint(new Matrix(new double[][]{new double[]{-1, 1}}), Vectors.dense(new double[]{0.2})))
+                        .augmentedLagrangianParameter(1)
+                        .maximumNumberOfIterations(100)
+                        .checkForObjectiveConvergence(false)
+                        .checkForGradientConvergence(false)
+                        .checkForPointConvergence(false)
+                        .loggingLevel(5)
+                        .build();
+
+        Vector result = consensusADMMSolver.solve();
+        System.out.println(result.get(0));
+        System.out.println(result.get(1));
+        System.out.println('\n');
     }
 }
