e2fa7a9c444e2284c94aa7bedbcace9f43ffdb08
Anthony Platanios <anthonyplat719791@hotmail.com>
2015-05-07 12:45:01 -0400

    Added support for different kinds of logic grounding methods.

--- learn/src/main/java/org/platanios/learn/logic/grounding/GreedyGrounding.java
+++ learn/src/main/java/org/platanios/learn/logic/grounding/GreedyGrounding.java
@@ -3,21 +3,25 @@ package org.platanios.learn.logic.grounding;
 import org.platanios.learn.logic.LogicManager;
 import org.platanios.learn.logic.formula.Atom;
 import org.platanios.learn.logic.formula.Formula;
+import org.platanios.learn.logic.formula.Negation;
 
 import java.util.List;
 
 /**
  * @author Emmanouil Antonios Platanios
  */
-public class ExhaustiveGrounding<T, R> extends Grounding<T, R> {
-    public ExhaustiveGrounding(LogicManager<T, R> logicManager) {
+public class GreedyGrounding<T, R> extends Grounding<T, R> {
+    public GreedyGrounding(LogicManager<T, R> logicManager) {
         super(logicManager);
     }
 
     @Override
     boolean pruneGroundingAndSetCurrentPredicateTruthValue(Formula<T> formula, List<R> disjunctionComponentsSoFar) {
         if (currentPredicateTruthValue == null)
-            disjunctionComponentsSoFar.add(logicManager.logic().falseValue());
+            if (formula instanceof Negation) // This is the important thing that PSL is doing while considering only the body variables.
+                return true;
+            else
+                disjunctionComponentsSoFar.add(logicManager.logic().falseValue());
         else if (formula instanceof Atom)
             disjunctionComponentsSoFar.add(currentPredicateTruthValue);
         else
---
+++ learn/src/main/java/org/platanios/learn/logic/grounding/Grounding.java
@@ -0,0 +1,164 @@
+package org.platanios.learn.logic.grounding;
+
+import org.platanios.learn.logic.LogicManager;
+import org.platanios.learn.logic.formula.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @author Emmanouil Antonios Platanios
+ */
+public abstract class Grounding<T, R> {
+    final LogicManager<T, R> logicManager;
+
+    List<R> groundingTruthValues = new ArrayList<>();
+    List<Variable<T>> groundedVariables = new ArrayList<>();
+    List<List<T>> partialVariableGroundings = new ArrayList<>();
+    List<List<GroundedPredicate<T, R>>> groundedPredicates = new ArrayList<>();
+    R currentPredicateTruthValue;
+
+    public Grounding(LogicManager<T, R> logicManager) {
+        this.logicManager = logicManager;
+        currentPredicateTruthValue = logicManager.logic().falseValue();
+    }
+
+    public void ground(Formula<T> formula) {
+        ground(formula.toDisjunctiveNormalForm(), 0);
+    }
+
+    @SuppressWarnings("unchecked")
+    private void ground(Formula<T> formula, int callNumber) {
+        if (callNumber != 0 && partialVariableGroundings.size() == 0)
+            return;
+        if (formula instanceof Atom || formula instanceof Negation) {
+            List<Variable<T>> argumentVariables = formula
+                    .getVariables()
+                    .stream()
+                    .collect(Collectors.toList());
+            List<Variable<T>> newGroundedVariables = new ArrayList<>(groundedVariables);
+            List<R> candidateGroundingTruthValues = new ArrayList<>(groundingTruthValues);
+            List<List<T>> candidateVariableGroundings = new ArrayList<>(partialVariableGroundings);
+            List<List<GroundedPredicate<T, R>>> candidateGroundedPredicates = new ArrayList<>(groundedPredicates);
+            argumentVariables.stream()
+                    .filter(argumentVariable -> !groundedVariables.contains(argumentVariable))
+                    .forEach(argumentVariable -> {
+                        candidateGroundingTruthValues.clear();
+                        candidateVariableGroundings.clear();
+                        candidateGroundedPredicates.clear();
+                        newGroundedVariables.add(argumentVariable);
+                        if (partialVariableGroundings.size() > 0) {
+                            for (int index = 0; index < partialVariableGroundings.size(); index++) {
+                                for (T variableValue : logicManager.getVariableValues(argumentVariable)) {
+                                    List<T> variableGrounding = new ArrayList<>(partialVariableGroundings.get(index));
+                                    variableGrounding.add(variableValue);
+                                    candidateGroundingTruthValues.add(groundingTruthValues.get(index));
+                                    candidateVariableGroundings.add(variableGrounding);
+                                    candidateGroundedPredicates.add(new ArrayList<>(groundedPredicates.get(index)));
+                                }
+                            }
+                        } else {
+                            for (T variableValue : logicManager.getVariableValues(argumentVariable)) {
+                                List<T> variableGrounding = new ArrayList<>();
+                                variableGrounding.add(variableValue);
+                                candidateGroundingTruthValues.add(logicManager.logic().falseValue());
+                                candidateVariableGroundings.add(variableGrounding);
+                                candidateGroundedPredicates.add(new ArrayList<>());
+                            }
+                        }
+                        groundingTruthValues = new ArrayList<>(candidateGroundingTruthValues);
+                        partialVariableGroundings = new ArrayList<>(candidateVariableGroundings);
+                        groundedPredicates = new ArrayList<>(candidateGroundedPredicates);
+                    });
+            List<R> truthValues = new ArrayList<>(groundingTruthValues);
+            groundedVariables = newGroundedVariables;
+            groundingTruthValues = new ArrayList<>();
+            partialVariableGroundings = new ArrayList<>();
+            groundedPredicates = new ArrayList<>();
+            for (int candidateIndex = 0; candidateIndex < candidateGroundedPredicates.size(); candidateIndex++) {
+                List<T> candidateVariableGrounding = candidateVariableGroundings.get(candidateIndex);
+                Map<Variable<T>, T> variableAssignments = new HashMap<>();
+                for (int variableIndex = 0; variableIndex < groundedVariables.size(); variableIndex++)
+                    variableAssignments.put(groundedVariables.get(variableIndex), candidateVariableGrounding.get(variableIndex));
+                List<R> disjunctionComponentsSoFar = new ArrayList<>();
+                disjunctionComponentsSoFar.add(truthValues.get(candidateIndex));
+                if (formula instanceof Atom)
+                    currentPredicateTruthValue = formula.evaluate(logicManager, variableAssignments);
+                else
+                    currentPredicateTruthValue =
+                            ((Negation<T>) formula).getFormula().evaluate(logicManager, variableAssignments);
+                if (!pruneGroundingAndSetCurrentPredicateTruthValue(formula, disjunctionComponentsSoFar)) {
+                    groundingTruthValues.add(currentPredicateTruthValue);
+                    partialVariableGroundings.add(candidateVariableGrounding);
+                    GroundedPredicate<T, R> groundedPredicate;
+                    Predicate<T> predicate;
+                    if (formula instanceof Atom)
+                        predicate = ((Atom<T>) formula).getPredicate();
+                    else
+                        predicate = ((Atom<T>) ((Negation<T>) formula).getFormula()).getPredicate();
+                    candidateVariableGrounding = new ArrayList<>();
+                    for (Variable<T> variable : formula.getOrderedVariables())
+                        candidateVariableGrounding.add(variableAssignments.get(variable));
+                    if (logicManager.groundedPredicateExists(predicate,
+                                                             candidateVariableGrounding)) {
+                        groundedPredicate = logicManager.getGroundedPredicate(
+                                predicate,
+                                candidateVariableGrounding
+                        );
+                    } else {
+                        groundedPredicate = logicManager.addGroundedPredicate(
+                                predicate,
+                                candidateVariableGrounding //, logicManager.logic().falseValue()
+                        );
+                    }
+                    candidateGroundedPredicates.get(candidateIndex).add(groundedPredicate);
+                    groundedPredicates.add(candidateGroundedPredicates.get(candidateIndex));
+                }
+            }
+        } else if (formula instanceof Conjunction) {
+            if (callNumber == 0)
+                throw new IllegalStateException("The formula being grounded was not converted to valid disjunctive " +
+                                                        "normal form for some unknown reason.");
+
+            throw new UnsupportedOperationException();
+//            int numberOfComponents = ((Conjunction<T>) formula).getNumberOfComponents();
+//            for (int componentIndex = 0; componentIndex < numberOfComponents; componentIndex++) {
+//                Formula<T> componentFormula = ((Conjunction<T>) formula).getComponent(componentIndex);
+//                if (componentFormula instanceof Atom || componentFormula instanceof Negation) {
+//                    groundAtomOrNegation(formula, truthValues, candidateVariableGroundings, candidatePredicateGroundings);
+//                } else {
+//                    throw new IllegalStateException("The formula being grounded was not converted to valid disjunctive " +
+//                                                            "normal form for some unknown reason.");
+//                }
+//            }
+        } else if (formula instanceof Disjunction) {
+            if (callNumber != 0)
+                throw new IllegalStateException("The formula being grounded was not converted to valid disjunctive " +
+                                                        "normal form for some unknown reason.");
+
+            int numberOfComponents = ((Disjunction<T>) formula).getNumberOfComponents();
+            for (int componentIndex = 0; componentIndex < numberOfComponents; componentIndex++) {
+                Formula<T> componentFormula = ((Disjunction<T>) formula).getComponent(componentIndex);
+                if (componentFormula instanceof Atom || componentFormula instanceof Negation) {
+                    ground(componentFormula, callNumber++);
+                } else {
+                    throw new IllegalStateException("The formula being grounded was not converted to valid disjunctive " +
+                                                            "normal form for some unknown reason.");
+                }
+            }
+        } else {
+            throw new IllegalStateException("The formula being grounded was not converted to valid disjunctive " +
+                                                    "normal form for some unknown reason.");
+        }
+    }
+
+    abstract boolean pruneGroundingAndSetCurrentPredicateTruthValue(Formula<T> formula,
+                                                                    List<R> disjunctionComponentsSoFar);
+
+    public List<List<GroundedPredicate<T, R>>> getGroundedPredicates() {
+        return groundedPredicates;
+    }
+}
